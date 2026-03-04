9cf832443bd249fd9b8e0707afc38a8168372f69
JanFPV <118380037+JanFPV@users.noreply.github.com>
2026-03-04 18:55:08 +0100

    Primeros elementos visuales

---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,8 @@
+function Favorites() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-white">Mis Favoritos</h1>
+    </div>
+  )
+}
+export default Favorites;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-white">Catálogo de Películas</h1>
+      <p className="text-secondary">Próximamente portadas...</p>
+    </div>
+  )
+}
+export default Home;
