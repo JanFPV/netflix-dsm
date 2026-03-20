bd051f2389c60e961bc9abf185ff0fd4c0f569cb
JanFPV <118380037+JanFPV@users.noreply.github.com>
2026-03-20 12:21:20 +0100

    Eliminar comentarios del archivo generado por firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -17,7 +12,6 @@ const firebaseConfig = {
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getDatabase(app);
